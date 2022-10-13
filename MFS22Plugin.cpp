bb6f81c285803f124572107351ad0fba690f885b
Mohamed sadiq ikbal <mohamedsadiq.ikbal@gmail.com>
2022-10-13 22:55:22 +0200

    Sockets added and data flow tested from Microsoft sim -> plugin -> cueing -> SP7

--- MFS22Plugin.cpp
+++ MFS22Plugin.cpp
@@ -11,28 +11,31 @@ int main()
 	}
 	std::cout << "Connected to MFS22: Data Extraction Started! \n";
 
+	
 
 	//Requesting Data
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ACCELERATION BODY X", "Feet (ft) per second squared"); //Default is float64
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ACCELERATION BODY Y", "Feet (ft) per second squared");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ACCELERATION BODY Z", "Feet (ft) per second squared");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "PLANE BANK DEGREES", "Radians");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "PLANE PITCH DEGREES", "Radians");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "PLANE HEADING DEGREES TRUE", "Radians");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ROTATION VELOCITY BODY X", "Feet (ft) per second");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ROTATION VELOCITY BODY Y", "Feet (ft) per second");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ROTATION VELOCITY BODY Z", "Feet (ft) per second");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "GROUND VELOCITY", "Knots");
-	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "TOTAL WEIGHT", "Pounds");
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ANIMATION DELTA TIME", "Seconds", SIMCONNECT_DATATYPE_FLOAT32); //Default is float64
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ACCELERATION BODY X", "Feet (ft) per second squared", SIMCONNECT_DATATYPE_FLOAT32); //Default is float64
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ACCELERATION BODY Y", "Feet (ft) per second squared", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ACCELERATION BODY Z", "Feet (ft) per second squared", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "PLANE BANK DEGREES", "Radians", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "PLANE PITCH DEGREES", "Radians", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "PLANE HEADING DEGREES TRUE", "Radians", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ROTATION VELOCITY BODY X", "Feet (ft) per second", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ROTATION VELOCITY BODY Y", "Feet (ft) per second", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "ROTATION VELOCITY BODY Z", "Feet (ft) per second", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "GROUND VELOCITY", "Knots", SIMCONNECT_DATATYPE_FLOAT32);
+	hr = SimConnect_AddToDataDefinition(hSimConnect, DEFINITION_1, "TOTAL WEIGHT", "Pounds", SIMCONNECT_DATATYPE_FLOAT32);
 
 	hr = SimConnect_RequestDataOnSimObject(hSimConnect, REQUEST_1, DEFINITION_1, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_SIM_FRAME);
 
 	//SET UP SOCKETS
-
+	initSocketSetup();
 
 	//Process incoming SimConnect server messages while the app is running
 	while (!quit)
 	{
+		startSocketSession();
 		SimConnect_CallDispatch(hSimConnect, MyDispatchProc, NULL);
 		Sleep(1);
 	}
@@ -43,6 +46,8 @@ int main()
 		return -2;
 	}
 	hSimConnect = nullptr;
+
+	cleanUpSocket();
 	return 0;
 }
 
@@ -60,12 +65,18 @@ void CALLBACK MyDispatchProc(SIMCONNECT_RECV* pData, DWORD cbData, void * pConte
 		{
 		case REQUEST_1:
 		{
+
 			SimResponse* pS = (SimResponse*)&pObjData->dwData;
-			//SEND SOCKET
-			// Now you have the data
-			std::cout  << "\ax: " << pS->ax
-				<< " - ay: " << pS->ay
-				<< " - az: " << pS->az << std::setprecision(4)  << std::endl << std::flush;
+			//SEND DATA THROUGH SOCKET
+			MPD_CalculateMotionData(pS);
+			float t = MPD_MotionData[0];
+			float hz = 1.0f / (pS->dt);
+
+
+			if (int res = sendto(client_socket, (char*)MPD_MotionData, sizeof(MPD_MotionData), 0, (sockaddr*)&server, sizeof(sockaddr_in)) == SOCKET_ERROR)
+				printf("sendto() failed with error code: %d", WSAGetLastError());
+			else 
+				printf("send ok [%d] [%dHz]\n", res, (int)hz);
 			
 			break;
 		}
@@ -82,4 +93,139 @@ void CALLBACK MyDispatchProc(SIMCONNECT_RECV* pData, DWORD cbData, void * pConte
 		break;
 
 	}
+}
+
+
+//-- 
+void initSocketSetup()
+{
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+
+	/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+	wVersionRequested = MAKEWORD(2, 2);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", err);
+		return;
+	}
+
+
+	// nc3 (asss new with var)
+	memset(MPD_MotionData, 0, sizeof(MPD_MotionData));
+
+	// create socket : cueing
+	if ((client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) // <<< UDP socket
+	{
+		printf("socket() failed with error code: %d", WSAGetLastError());
+		//return 2;
+	}
+	// setup address structure : cueing
+	// server comm start -- 
+	memset((char*)&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(UDP_PORT);
+	server.sin_addr.S_un.S_addr = inet_addr(SERVER);
+	
+
+	//Rasberry Pi "172.16.1.1" 
+
+	// create socket : controller
+	if ((client_socket_ctrl = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) // <<< UDP socket
+	{
+		printf("socket() failed with error code: %d", WSAGetLastError());
+		//return 2;
+	}
+
+	//setup address structure : controller
+	memset((char*)&server_ctrl, 0, sizeof(server_ctrl));
+	server_ctrl.sin_family = AF_INET;
+	server_ctrl.sin_port = htons(CTRL_UDP_PORT);
+	server_ctrl.sin_addr.S_un.S_addr = inet_addr(SERVER);
+
+	firstRun = true;
+}
+
+void cleanUpSocket()
+{
+	strcpy(dataControl, "STOP");
+	if (sendto(client_socket_ctrl, (char*)dataControl, sizeof(dataControl), 0, (sockaddr*)&server_ctrl, sizeof(sockaddr_in)) == SOCKET_ERROR)
+	{
+		printf("sendto() failed with error code: %d", WSAGetLastError());
+	}
+	
+	closesocket(client_socket);
+
+	//send control signal to disconnect
+	strcpy(dataControl, "DISC");
+	if (sendto(client_socket_ctrl, (char*)dataControl, sizeof(dataControl), 0, (sockaddr*)&server_ctrl, sizeof(sockaddr_in)) == SOCKET_ERROR)
+	{
+		printf("sendto() failed with error code: %d", WSAGetLastError());
+		//return 3;
+	}
+
+	//send control signal to quit
+	strcpy(dataControl, "QUIT");
+	if (sendto(client_socket_ctrl, (char*)dataControl, sizeof(dataControl), 0, (sockaddr*)&server_ctrl, sizeof(sockaddr_in)) == SOCKET_ERROR)
+	{
+		printf("sendto() failed with error code: %d", WSAGetLastError());
+		//return 3;
+	}
+
+
+	closesocket(client_socket_ctrl);
+	WSACleanup();
+}
+
+
+void startSocketSession()
+{
+	if (firstRun)
+	{
+		// send control signal to connect 
+		strcpy(dataControl, "CONN");
+		if (sendto(client_socket_ctrl, (char*)dataControl, sizeof(dataControl), 0, (sockaddr*)&server_ctrl, sizeof(sockaddr_in)) == SOCKET_ERROR)
+		{
+			printf("sendto() failed with error code: %d", WSAGetLastError());
+			//return 3;
+		}
+		firstRun = false;
+	}
+
+	strcpy(dataControl, "STRT");
+	if (sendto(client_socket_ctrl, (char*)dataControl, sizeof(dataControl), 0, (sockaddr*)&server_ctrl, sizeof(sockaddr_in)) == SOCKET_ERROR)
+	{
+		printf("sendto() failed with error code: %d", WSAGetLastError());
+		//return 3;
+	}
+}
+
+void MPD_CalculateMotionData(SimResponse* pS)
+{
+	// Store the results in an array so that we can easily send it.
+	//CONVERT UNITS HERE
+	current_time += pS->dt;
+	MPD_MotionData[0] = current_time;
+	MPD_MotionData[1] = pS->ax;
+	MPD_MotionData[2] = pS->ay;
+	MPD_MotionData[3] = pS->az;
+	MPD_MotionData[4] = pS->roll;
+	MPD_MotionData[5] = pS->pitch;
+	MPD_MotionData[6] = pS->yaw;
+	MPD_MotionData[7] = pS->vroll;
+	MPD_MotionData[8] = pS->vpitch;
+	MPD_MotionData[9] = pS->vyaw;
+	MPD_MotionData[10] = pS->GS;
+	MPD_MotionData[11] = pS->mass;
+	MPD_MotionData[12] = 0.0f;
+	MPD_MotionData[13] = 0.0f;
+	MPD_MotionData[14] = 0.0f;
+	MPD_MotionData[15] = 0.0f;
+	MPD_MotionData[16] = 0.0f;
+	MPD_MotionData[17] = 0.0f;
+	MPD_MotionData[18] = 0.0f;
 }
--- MFS22Plugin.h
+++ MFS22Plugin.h
@@ -1,7 +1,13 @@
 #ifndef MFS22PLUGIN_H_
 #define MFS22PLUGIN_H_
 
-#pragma once
+#pragma comment(lib, "Ws2_32.lib")
+#pragma warning(disable:4996) 
+
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+
 #include <iostream>
 #include <windows.h>
 #include <iomanip>
@@ -26,19 +32,57 @@ static enum DATA_REQUEST_ID
 										//"ph_x", "ph_y", "ph_z", "ph_roll", "ph_pitch", "ph_yaw", "zulu_time" }; //nc4 (add header)
 struct SimResponse
 {
-	double ax; //ACCELERATION BODY X
-	double ay; //ACCELERATION BODY Y
-	double az; //ACCELERATION BODY Z
-	double roll;//PLANE BANK DEGREES [Radians]
-	double pitch;//PLANE PITCH DEGREES [Radians]
-	double yaw;//PLANE HEADING DEGREES TRUE	 [Radians]
-	double vroll;//ROTATION VELOCITY BODY X
-	double vpitch;//ROTATION VELOCITY BODY Y
-	double vyaw;//ROTATION VELOCITY BODY Z
-	double GS;//GROUND VELOCITY
-	double mass;// TOTAL WEIGHT[Pounds]
+	float dt; //ANIMATION DELTA TIME [Seconds]
+	float ax; //ACCELERATION BODY X [Feet (ft) per second squared]
+	float ay; //ACCELERATION BODY Y
+	float az; //ACCELERATION BODY Z
+	float roll;//PLANE BANK DEGREES [Radians]
+	float pitch;//PLANE PITCH DEGREES [Radians]
+	float yaw;//PLANE HEADING DEGREES TRUE	 [Radians]
+	float vroll;//ROTATION VELOCITY BODY X
+	float vpitch;//ROTATION VELOCITY BODY Y
+	float vyaw;//ROTATION VELOCITY BODY Z
+	float GS;//GROUND VELOCITY
+	float mass;// TOTAL WEIGHT[Pounds]
 };
 
 void CALLBACK MyDispatchProc(SIMCONNECT_RECV* pData, DWORD cbData, void* pContext);
 
+
+
+//------ Socket setup
+#define SERVER "127.0.0.1"  // or "localhost" - ip address of UDP server
+#define UDP_PORT 22608
+#define NUM_DATA 19 // nc1
+
+
+#define CTRL_UDP_PORT 22609 // This is an abstract layer to connect to sp7 and disconnect
+char dataControl[5]{}; //"QUIT" "CONN" "STRT" "STOP" "STAT"
+
+// Used to store calculated motion data
+float MPD_MotionData[NUM_DATA];
+
+static float current_time = 0.0f;
+
+//SOCKET sock;
+//struct sockaddr_in server;
+
+// create socket : controller
+sockaddr_in server_ctrl;
+int client_socket_ctrl;
+
+sockaddr_in server;
+int client_socket;
+bool firstRun;
+
+//------
+
+
+void initSocketSetup();
+
+void cleanUpSocket();
+
+void startSocketSession();
+
+void MPD_CalculateMotionData(SimResponse* pS);
 #endif // !MFS22PLUGIN_H_
